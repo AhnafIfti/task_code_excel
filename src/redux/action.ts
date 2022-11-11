feb16c744e930a0abc313cd63fe118cfa4ec397c
AhnafIfti <ahannafifti@gmail.com>
2022-11-11 21:20:25 +0600

    removing redux

--- src/redux/action.ts
+++
@@ -1,6 +0,0 @@
-import {authenticationType} from './types';
-
-export const setIsAuthenticated = (authentication:any) => ({
-    type: authenticationType.IS_AUTHENTIACTED,
-    payload: authentication
-})
--- src/redux/root-reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import AuthenticationReducer from "./authenticationReducer";
-import userReducer from '../feature/mostEmailed/mostEmailedSlice'
-
-export default combineReducers({
-  authentication: AuthenticationReducer,
-  user: userReducer
-});
